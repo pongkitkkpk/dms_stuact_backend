bc822c84048789f365cd6611368b33b12bc96ad1
pongkitkkpk <pongkitkim@gmail.com>
2024-01-30 16:41:44 +0700

    delete role ok

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
 
-
 app.use(cors());
 app.use(express.json());
 
@@ -12,40 +11,48 @@ const db = mysql.createConnection({
     host: "localhost",
     password: "",
     database: "usersystem"
-})
+});
 
 app.get('/users', (req, res) => {
     db.query("SELECT * FROM users", (err, result) => {
         if (err) {
-            console.log(err)
+            console.log(err);
+            res.status(500).send(err); // Handle the error and send an appropriate response
         } else {
             res.send(result);
         }
     });
-})
+});
 
+app.delete('/deleteUser/:id', (req, res) => { // Added a leading slash
+    const id = req.params.id;
+    db.query("DELETE FROM users WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err); // Handle the error and send an appropriate response
+        } else {
+            res.send(result);
+        }
+    });
+});
 
-app.post('/create', (req, res) => {
-    const idStudent = req.body.idStudent;
-    const nameStudent = req.body.nameStudent;
-    const department = req.body.department;
-    const position = req.body.position;
-    const campus = req.body.campus;
-    const clubName = req.body.clubName;
-    const codeClub = req.body.codeClub;
+app.post('/createUser', (req, res) => {
+    const { idStudent, nameStudent, department, position, campus, clubName, codeClub } = req.body;
 
-    db.query("INSERT INTO users (idStudent,nameStudent,department,position,campus,clubName,codeClub) VALUES(?,?,?,?,?,?,?)",
+    db.query(
+        "INSERT INTO users (idStudent, nameStudent, department, position, campus, clubName, codeClub) VALUES (?, ?, ?, ?, ?, ?, ?)",
         [idStudent, nameStudent, department, position, campus, clubName, codeClub],
         (err, result) => {
             if (err) {
-                console.log(err)
+                console.log(err);
+                res.status(500).send(err); // Handle the error and send an appropriate response
             } else {
-                res.send("Values Inserted")
+                res.send("Values Inserted");
             }
-        })
+        }
+    );
+});
 
-})
-
-app.listen('3001', () => {
-    console.log("Server is running on port 3001")
-})
+app.listen(3001, () => { // Removed quotes around 3001
+    console.log("Server is running on port 3001");
+});
