fdec12c4373ec4ceb810e5c45fb073b9e73f53fc
pongkitkkpk <pongkitkim@gmail.com>
2024-02-05 21:47:30 +0700

    connect table is ok fix form frontend

--- index.js
+++ index.js
@@ -53,6 +53,97 @@ app.post('/createUser', (req, res) => {
     );
 });
 
+app.post('/createProject', (req, res) => {
+    const {
+        project_name,
+        project_number,
+        codeclub,
+        yearly,
+        yearly_count,
+        responsible_agency,
+        academic_year,
+        advisor_name,
+        person1_name,
+        person1_contact,
+        person2_name,
+        person2_contact,
+        person3_name,
+        person3_contact,
+        principles_and_reasons,
+        objective1,
+        objective2,
+        objective3,
+        project_type1,
+        project_type2,
+        project_type3,
+        is_newproject,
+        is_continueproject,
+        number_of_staff,
+        number_of_staffstudent,
+        number_of_joinstudent,
+        etc_typename1,
+        number_of_etc1,
+        etc_typename2,
+        number_of_etc2,
+        location1,
+        location2,
+        location3,
+        startDate,
+        endDate
+    } = req.body;
+
+    db.query(
+        "INSERT INTO projects (project_name, project_number, codeclub, startDate, endDate) VALUES ('asdfasdf', 'B', 'A', NOW(), NOW())",
+        // "INSERT INTO projects (project_name, project_number, codeclub, yearly, yearly_count, responsible_agency, academic_year, advisor_name, person1_name, person1_contact, person2_name, person2_contact, person3_name, person3_contact, principles_and_reasons, objective1, objective2, objective3, project_type1, project_type2, project_type3, is_newproject, is_continueproject, number_of_staff, number_of_staffstudent, number_of_joinstudent, etc_typename1, number_of_etc1, etc_typename2, number_of_etc2, location1, location2, location3,startDate,endDate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW())",
+        [
+            project_name,
+            project_number,
+            codeclub,
+            yearly,
+            yearly_count,
+            responsible_agency,
+            academic_year,
+            advisor_name,
+            person1_name,
+            person1_contact,
+            person2_name,
+            person2_contact,
+            person3_name,
+            person3_contact,
+            principles_and_reasons,
+            objective1,
+            objective2,
+            objective3,
+            project_type1,
+            project_type2,
+            project_type3,
+            is_newproject,
+            is_continueproject,
+            number_of_staff,
+            number_of_staffstudent,
+            number_of_joinstudent,
+            etc_typename1,
+            number_of_etc1,
+            etc_typename2,
+            number_of_etc2,
+            location1,
+            location2,
+            location3,
+            startDate,
+            endDate
+        ],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send(err); // Handle the error and send an appropriate response
+            } else {
+                res.send("Values Inserted");
+            }
+        }
+    );
+});
+
+
 app.listen(3001, () => { // Removed quotes around 3001
     console.log("Server is running on port 3001");
 });
