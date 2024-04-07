7a275403fc4c33dc98734a24d8d18cb9469e5163
pongkitkkpk <pongkitkim@gmail.com>
2024-04-07 23:42:18 +0700

    ccan up todatabase

--- src/studentRoutes.js
+++ src/studentRoutes.js
@@ -661,7 +661,7 @@ router.post('/project/p_timestep/create/:id_project', async (req, res) => {
             ]
         );
 
-        res.status(200).json({ message: `p_timestep updated successfully ${codeclub} yearly${yearly_countsketch}` });   
+        res.status(200).json({ message: `p_timestep updated successfully ${codeclub} yearly${yearly_countsketch}` });
         db.query(
             "INSERT INTO p_budget (id_projects,codeclub,yearly_countsketch) VALUES (?,?,?)",
             [id_projects, codeclub, yearly_countsketch],
@@ -671,17 +671,64 @@ router.post('/project/p_timestep/create/:id_project', async (req, res) => {
                     res.status(500).send(err); // Handle the error and send an appropriate response
                     return;
                 }
-
-                // If both insertions are successful, send a success response
-               
             }
         );
-
-
     } catch (error) {
         console.error("Error updating p_timestep:", error);
         res.status(500).json({ error: "Database error" });
     }
 });
+router.post('/project/p_budget/create/:id_projects', async (req, res) => {
+    try {
+        const {
+            id_projects,
+            codeclub,
+            yearly_countsketch,
+            listA1,
+            listA2,
+            listA3
+            
+        } = req.body;
+        console.log(codeclub)
+        db.query(
+            "UPDATE p_budget SET  codeclub = ?,yearly_countsketch = ?, listA1 = ?, listA2=?, listA3=?,  WHERE id_projects=?",
+            [
+                codeclub,
+                yearly_countsketch,
+                listA1,
+                listA2,
+                listA3,
+                id_projects
+                
+            ],
+            async (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send("Error updating data in the database"); // Handle the error and send an appropriate response
+                } else {
+                    console.log("up to database compleate")
+                }
 
+
+                // db.query("INSERT INTO p_timestep (id_projects,codeclub,yearly_countsketch) VALUES (?,?,?)",
+                //     [id_projects, codeclub, yearly_countsketch],
+                //     (err) => {
+                //         if (err) {
+                //             console.error(err);
+                //             return res.status(500).send("Error inserting data into the database"); // Handle the error and send an appropriate response
+                //         }
+
+                //         res.status(200).send("Data updated and inserted successfully");
+                //     }
+                // );
+            }
+        );
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error); // Handle the error and send an appropriate response
+    }
+
+
+})
 module.exports = router;
