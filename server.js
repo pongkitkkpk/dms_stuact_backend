ec431b77be67620b0cd84c2698f82535ea25f3e1
pongkitkkpk <pongkitkim@gmail.com>
2024-05-05 19:58:55 +0700

    not build

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 
 // if(process.env.NODE_ENV == 'production'){
