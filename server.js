3bf797d8a2fcab943e945a52da32d62dfede6358
pongkitkkpk <pongkitkim@gmail.com>
2024-05-05 17:13:51 +0700

    build not finnish version

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+// if(process.env.NODE_ENV == 'production'){
+//   host = 'usersystem'
+// }
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
